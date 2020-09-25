3ef03e2451c6ad72a24bff542b0caf51790fcea3
金 <1211050148@qq.com>
2020-09-25 17:09:46 +0800

    佣金方案配置

--- src/pages/ninehome/commission/commission.vue
+++ src/pages/ninehome/commission/commission.vue
@@ -7,20 +7,20 @@
         <div class="title">添加</div>
         <div class="operate-btn">
           <div class="search-box">
-            <input type="text" placeholder="请输入会员ID" v-model="memberId" />
+            <input type="text" placeholder="请输入方案名称" v-model="cfgName" />
             <img src="../../../assets/nine/search.png" class="search-img" />
           </div>
-          <div class="search-user">查询</div>
+          <div class="search-user" @click="search()">查询</div>
         </div>
       </div>
       <!--表格-->
       <div class="reset-scroll-style">
-        <el-table :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
-          <el-table-column show-overflow-tooltip label="方案名称" width="100" prop="packageName" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="日结方案" prop="dailyPlan" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="合作分成方案" prop="sharingScheme" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="月结方案" prop="statementScheme" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="用户注册默认" width="100" prop="signup" align="center"></el-table-column>
+        <el-table v-if="nullTable==false" :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
+          <el-table-column show-overflow-tooltip label="方案名称" width="100" prop="cfgName" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="日结方案" prop="dayCommission" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="合作分成方案" prop="singleCommission" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="月结方案" prop="monthCommission" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="用户注册默认" width="100" prop="isDefault" :formatter="formatter" align="center"></el-table-column>
           <el-table-column label="操作" align="center">
             <template slot-scope="scope">
               <div class="operation">
@@ -31,8 +31,15 @@
             </template>
           </el-table-column>
         </el-table>
+        <el-table v-if="nullTable==true" :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
+          <el-table-column show-overflow-tooltip label="方案名称" width="100" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="日结方案" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="合作分成方案" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="月结方案" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="用户注册默认" width="100" align="center"></el-table-column>
+        </el-table>
       </div>
-      <div class="pagination">
+      <div class="pagination" v-if="nullTable==false">
         <el-pagination :current-page.sync="currentPage" layout="prev, pager, next" :page-size="pageSzie" :pager-count="5" :total="total" @current-change="handleCurrentChange"></el-pagination>
       </div>
     </div>
@@ -47,59 +54,9 @@ export default {
   },
   data() {
     return {
-      tableData: [
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        },
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        },
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        },
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        },
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        },
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        },
-        {
-          packageName: "总部(平台)",
-          dailyPlan: "交易佣金率：0‰；管理费成交率：0%",
-          sharingScheme: "交易佣金率：0‰；管理费成交率：0%；盈利分成成交率：0%",
-          statementScheme: "交易佣金率：0‰；管理费成交率：0%",
-          signup: "-"
-        }
-      ],
+      tableData: [],
       colorBool: false,
-      memberId: "",
+      cfgName: "",
       pageSzie: 31,
       currentPage: 1,
       total: 10,
@@ -126,31 +83,47 @@ export default {
     }
   },
   watch: {
-    memberId: {
-      handler(newVal, oldVal) {
-        this.currentPage = 1;
-        // this.getFundAccount();
-      },
-      deep: true
-    }
+    // cfgName: {
+    //   handler(newVal, oldVal) {
+    //     this.currentPage = 1;
+    //     this.getFundAccount();
+    //   },
+    //   deep: true
+    // }
   },
   created() {
-    // this.getFundAccount();
+    this.getFundAccount();
   },
   methods: {
+    formatter(row, column) {
+      if (row) {
+        if (row.isDefault == 0) {
+          return "否";
+        } else if (row.isDefault == 1) {
+          return "是";
+        }
+      }
+    },
+    search() {
+      this.currentPage = 1;
+      this.getFundAccount();
+    },
     getFundAccount() {
       this.axios
-        .get("account/fund", {
-          params: {
-            search: this.memberId,
-            size: this.pageSzie,
-            page: this.currentPage
-          }
+        .post("/tn/mgr-api/commissionCfgs/pageQuery", {
+          pageSize: this.pageSzie,
+          pageNo: this.currentPage,
+          cfgName: this.cfgName
         })
         .then(res => {
-          console.log("getFundAccount>>", res.data.data);
-          if (res.data.code == 1) {
-            this.tableData = res.data.data.data;
+          console.log("getFundAccount>>", res.data);
+          if (res.data.code == 200) {
+            let rows = res.data.data.rows;
+            if (rows.length > 0) {
+              this.tableData = res.data.data.rows;
+            } else {
+              this.tableData = [];
+            }
             this.total = res.data.data.total;
           } else {
             this.tableData = [];
@@ -161,6 +134,8 @@ export default {
           } else {
             this.nullTable = false;
           }
+
+          console.log("我是最终结果", this.tableData);
         })
         .catch(err => {
           console.log(err);
