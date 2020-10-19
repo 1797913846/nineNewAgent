6eecd2b228958755a024db1dfa0b7657656a017f
金 <1211050148@qq.com>
2020-10-19 16:07:25 +0800

    资金池管理

--- src/pages/ninehome/pool/pool.vue
+++ src/pages/ninehome/pool/pool.vue
@@ -1,12 +1,12 @@
 <!--资金池管理-->
 <template>
-  <div class="bigestbox">
+  <div class="bigestbox pool">
     <topNav></topNav>
     <div class="container" @click="colorBool = false">
       <div class="template-top">
-        <div class="title">新增</div>
-        <div class="title" style="margin-left:10px;">编辑</div>
-        <div class="title" style="margin-left:10px;" @click="deleteAll">删除</div>
+        <div class="title" @click="add">新增</div>
+        <!-- <div class="title" style="margin-left:10px;">编辑</div> -->
+        <!-- <div class="title" style="margin-left:10px;" @click="deleteAll">删除</div> -->
         <div class="operate-btn">
           <!-- <div class="search-box">
             <input type="text" placeholder="请输入会员ID" v-model="id" />
@@ -33,10 +33,16 @@
       <!--表格-->
       <div class="reset-scroll-style">
         <el-table :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle" @selection-change="handleSelectionChange">
-          <el-table-column type="selection" width="55" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="分组id" prop="groupName" align="center"></el-table-column>
+          <el-table-column label="操作" align="center" width="180">
+            <template slot-scope="scope">
+              <div class="operation">
+                <span @click.stop="getEdit(scope.$index,scope.row)">修改</span>
+                <span @click.stop="deleteNow(scope.$index, scope.row)">删除</span>
+              </div>
+            </template>
+          </el-table-column>
           <el-table-column show-overflow-tooltip label="分组名称" prop="groupName" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="资金账户" prop="productCode" align="center"></el-table-column>
+          <el-table-column show-overflow-tooltip label="资金账户" prop="productCodes" align="center"></el-table-column>
         </el-table>
       </div>
       <div class="pagination">
@@ -44,24 +50,54 @@
       </div>
     </div>
     <!--新增-->
-    <!-- <div class="addForm">
+    <div class="addForm" v-if="addBool==true">
       <div class="addContent">
         <div class="title">
           <span class="tl">{{addTitle}}</span>
-          <span class="tr">关闭</span>
         </div>
         <el-form :inline="true" :model="formInline" ref="formInline" class="demo-form-inline">
           <el-form-item label="分组名称：">
             <el-input v-model="levelName" placeholder="分组名称"></el-input>
           </el-form-item>
+          <div class="codebox">
+            <span class="codetitle">资金账号：</span>
+            <div class="checkbox">
+              <el-checkbox-group v-model="checkList" align="left">
+                <el-checkbox v-for="(item,index) in productList" :key="index" :label="item.productcode">{{item.productname}}</el-checkbox>
+              </el-checkbox-group>
+            </div>
+          </div>
+          <div class="buybox" v-if="accountGroup.length>0">
+            <div class="buytitle">买入优先级：</div>
+            <div class="buycontent">
+              <el-table :border="true" :highlight-current-row="colorBool" :data="accountGroup" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" :cell-style="cellStyle" :header-cell-style="headerCellStyle" v-if="accountGroup.length>0">
+                <el-table-column label="序号" align="center" width="100">
+                  <template slot-scope="scope">
+                    <div class="operation">
+                      <span>{{scope.row.priority}}</span>
+                    </div>
+                  </template>
+                </el-table-column>
+                <el-table-column show-overflow-tooltip label="资金账号" prop="productCode" align="center"></el-table-column>
+                <el-table-column label="操作" align="center" width="180">
+                  <template slot-scope="scope">
+                    <div class="operation">
+                      <span @click.stop="set1(scope.$index,scope.row)">保存</span>
+                      <span @click.stop="set2(scope.$index, scope.row)">修改</span>
+                    </div>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+          </div>
           <br />
           <el-form-item>
-            <el-button type="primary">保存</el-button>
-            <el-button type="primary">取消</el-button>
+            <el-button type="primary" @click="saveNow('formInline')">{{addTitle}}</el-button>
+            <el-button type="primary" @click="closeadd('formInline')">取消</el-button>
           </el-form-item>
         </el-form>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -108,7 +144,15 @@ export default {
       nullTable: false,
       groupId: [],
       addTitle: "新增",
-      groupId: 0
+      groupId: 0,
+      addBool: false,
+      productPool: "",
+      productList: "",
+      checkList: [],
+      levelName: "",
+      formInline: {},
+      accountGroup: [],
+      realAccountGroup: []
     };
   },
   computed: {
@@ -131,27 +175,119 @@ export default {
     }
   },
   watch: {
-    keyword: {
+    checkList: {
       handler(newVal, oldVal) {
-        this.currentPage = 1;
-        this.getFundAccount();
+        this.accountGroup = [];
+        newVal.map(item => {
+          this.accountGroup.push({
+            priority: 0,
+            productCode: item
+          });
+        });
       },
       deep: true
     }
   },
   created() {
     this.getFundAccount();
-    this.getContent();
   },
   methods: {
-    getContent() {
+    add() {
+      this.addBool = true;
+      this.getContent(0);
+      this.addTitle = "新增";
+    },
+    set1(index, row) {
+      console.log("是我", index, row);
+      this.realAccountGroup.push(row);
+    },
+    set2(index, row) {},
+    closeadd(formName) {
+      this.$refs[formName].resetFields();
+      this.addBool = false;
+    },
+    saveNow(formName) {
+      console.log("我被勾中了", this.checkList);
+      if (this.checkList.length <= 0) {
+        this.$alert("必须勾选资金账号", "提示", {
+          confirmButtonText: "确定",
+          center: true,
+          type: "error"
+        });
+      } else {
+        if (this.realAccountGroup.length <= 0) {
+          this.$alert("请先保存买入优先级", "提示", {
+            confirmButtonText: "确定",
+            center: true,
+            type: "error"
+          });
+        } else {
+          let url, options;
+          if (this.addTitle == "新增") {
+            url = "tn/mgr-api/fund-pool/save";
+            options = {
+              groupName: this.levelName,
+              accountGroup: this.realAccountGroup
+            };
+          } else if (this.addTitle == "修改") {
+            url = "/tn/mgr-api/fund-pool/update";
+            options = {
+              groupId: this.groupId,
+              groupName: this.levelName,
+              accountGroup: this.realAccountGroup
+            };
+          }
+          this.axios
+            .post(url, options)
+            .then(res => {
+              if (res.data.code == 200) {
+                this.$alert(res.data.info, "提示", {
+                  confirmButtonText: "确定",
+                  center: true,
+                  type: "success"
+                });
+                this.getFundAccount();
+                this.addBool = false;
+              } else {
+                this.$alert(res.data.info, "提示", {
+                  confirmButtonText: "确定",
+                  center: true,
+                  type: "error"
+                });
+              }
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      }
+    },
+    //单个查询
+    getContent(groupId) {
       this.axios
         .post("/tn/mgr-api/fund-pool/detail", {
-          groupId: this.groupId
+          groupId: groupId
         })
         .then(res => {
           if (res.data.code == 200) {
-            
+            this.productPool = res.data.data.productPool;
+            this.productList = res.data.data.productList;
+            this.realAccountGroup = [];
+            this.productPool.accountGroup.map(item => {
+              this.realAccountGroup.push({
+                priority: item.priority,
+                productCode: item.productCode
+              });
+            });
+            // this.realAccountGroup = this.productPool;
+            this.levelName = this.productPool.groupName;
+            this.checkList = [];
+            //修改时候如果是选中的，那复选框就得勾选上
+            this.productList.map(item => {
+              if (item.fundPoolChecked == 1) {
+                this.checkList.push(item.productcode);
+              }
+            });
           } else {
             this.$alert(res.data.info, "提示", {
               confirmButtonText: "确定",
@@ -171,6 +307,38 @@ export default {
         groupIdList.push(item.groupId);
       });
       this.groupId = groupIdList;
+    },
+    getEdit(index, row) {
+      this.addBool = true;
+      let groupId = row.groupId;
+      this.getContent(groupId);
+      this.addTitle = "修改";
+    },
+    deleteNow(index, row) {
+      let groupId = row.groupId;
+      this.axios
+        .post("/tn/mgr-api/fund-pool/delete", {
+          groupId: groupId
+        })
+        .then(res => {
+          if (res.data.code == 200) {
+            this.$alert(res.data.info, "提示", {
+              confirmButtonText: "确定",
+              center: true,
+              type: "sucess"
+            });
+            this.getFundAccount();
+          } else {
+            this.$alert(res.data.info, "提示", {
+              confirmButtonText: "确定",
+              center: true,
+              type: "error"
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     deleteAll() {
       if (this.groupId.length <= 0) {
@@ -278,6 +446,37 @@ export default {
   float: right;
   cursor: pointer;
 }
+.codebox {
+}
+.codetitle {
+  font-size: 12px;
+  color: #606266;
+}
+.checkbox {
+  width: 390px;
+}
+.checkbox * {
+  color: #606266;
+}
+.buybox {
+  margin-top: 30px;
+}
+.buybox .buytitle {
+  font-size: 12px;
+  color: #606266;
+}
+.buycontent {
+}
 </style>
+<style>
+.pool .el-checkbox + .el-checkbox {
+  margin-left: 0px !important;
+  margin-right: 30px;
+}
+.pool .el-checkbox {
+  margin-right: 30px;
+}
+</style>
+
 
 
