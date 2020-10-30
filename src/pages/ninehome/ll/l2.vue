c09ff49a566a3b6e494591e95af9da5320e1bb4a
金 <1211050148@qq.com>
2020-10-30 16:08:59 +0800

    公告管理

--- src/pages/ninehome/ll/l2.vue
+++ src/pages/ninehome/ll/l2.vue
@@ -1,98 +1,66 @@
 <!--公告管理-->
 <template>
-  <div class="bigestbox">
-    <topNav></topNav>
-    <div class="container" @click="colorBool = false">
-      <div class="template-top">
-        <div class="title" @click="showAddNow">添加</div>
-        <div class="operate-btn">
-          <div class="search-box">
-            <input type="text" placeholder="请输入方案名称" v-model="cfgName" />
-            <img src="../../../assets/nine/search.png" class="search-img" />
-          </div>
-          <div class="search-user" @click="search()">查询</div>
+    <div class="bigestbox">
+        <topNav></topNav>
+        <div class="container" @click="colorBool = false">
+            <div class="template-top">
+                <div class="title" @click="showAddNow">添加</div>
+                <div class="operate-btn">
+                    <div class="search-box">
+                        <input type="text" placeholder="请输入标题" v-model="title" />
+                        <img src="../../../assets/nine/search.png" class="search-img" />
+                    </div>
+                    <div class="search-user" @click="search()">查询</div>
+                </div>
+            </div>
+            <!--表格-->
+            <div class="reset-scroll-style">
+                <el-table v-if="nullTable==false" :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
+                    <el-table-column show-overflow-tooltip label="标题" prop="title" align="center"></el-table-column>
+                    <el-table-column show-overflow-tooltip label="更新时间" prop="createTime" align="center"></el-table-column>
+                    <el-table-column show-overflow-tooltip label="操作" align="center"></el-table-column>
+                    <el-table-column label="操作" align="center">
+                        <template slot-scope="scope">
+                            <div class="operation">
+                                <span @click.stop="getContent(scope.$index, scope.row)" class="addSameClass ">修改</span>
+                                <span @click.stop="deleteNow(scope.$index, scope.row)" class="addSameClass ">删除</span>
+                            </div>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                <el-table v-if="nullTable==true" :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
+                    <el-table-column show-overflow-tooltip label="标题" align="center"></el-table-column>
+                    <el-table-column show-overflow-tooltip label="更新时间" align="center"></el-table-column>
+                </el-table>
+            </div>
+            <div class="pagination" v-if="nullTable==false">
+                <el-pagination :current-page.sync="currentPage" layout="prev, pager, next" :page-size="pageSize" :pager-count="5" :total="total" @current-change="handleCurrentChange"></el-pagination>
+            </div>
         </div>
-      </div>
-      <!--表格-->
-      <div class="reset-scroll-style">
-        <el-table v-if="nullTable==false" :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
-          <el-table-column show-overflow-tooltip label="方案名称" width="100" prop="cfgName" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="日结方案" prop="dayCommission" :formatter="formattera" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="合作分成方案" width="420" prop="singleCommission" :formatter="formatterb" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="月结方案" prop="monthCommission" :formatter="formatterc" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="用户注册默认" width="100" prop="isDefault" :formatter="formatter" align="center"></el-table-column>
-          <el-table-column label="操作" align="center">
-            <template slot-scope="scope">
-              <div class="operation">
-                <span @click.stop="getContent(scope.$index, scope.row)" class="addSameClass ">修改</span>
-                <span @click.stop="deleteNow(scope.$index, scope.row)" class="addSameClass ">删除</span>
-                <span v-if="scope.row.isDefault==0" @click.stop="setIt(scope.$index, scope.row)" class="addSameClass ">置为默认</span>
-                <span v-if="scope.row.isDefault==1" class="addSameClass ">默认方案</span>
-              </div>
-            </template>
-          </el-table-column>
-        </el-table>
-        <el-table v-if="nullTable==true" :border="true" :highlight-current-row="colorBool" :data="tableData" key="desingerTable" stripe class="user-table" style="width:100%;background-color:#ffffff;" height="600" :cell-style="cellStyle" :header-cell-style="headerCellStyle">
-          <el-table-column show-overflow-tooltip label="方案名称" width="100" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="日结方案" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="合作分成方案" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="月结方案" align="center"></el-table-column>
-          <el-table-column show-overflow-tooltip label="用户注册默认" width="100" align="center"></el-table-column>
-        </el-table>
-      </div>
-      <div class="pagination" v-if="nullTable==false">
-        <el-pagination :current-page.sync="currentPage" layout="prev, pager, next" :page-size="pageSize" :pager-count="5" :total="total" @current-change="handleCurrentChange"></el-pagination>
-      </div>
-    </div>
-    <!--表单-->
-    <div class="addForm" v-if="showAdd==true">
-      <div class="addContent">
-        <div class="title">
-          <span class="tl">{{addTitle}}</span>
-          <span class="tr" @click="closeAdd">X</span>
+        <!--表单-->
+        <div class="addForm" v-if="showAdd==true">
+            <div class="addContent">
+                <div class="title">
+                    <span class="tl">{{addTitle}}</span>
+                    <span class="tr" @click="closeAdd">X</span>
+                </div>
+                <el-form :inline="true" :model="formInline" ref="formInline" class="demo-form-inline">
+                    <el-form-item label="标题：">
+                        <el-input v-model="formInline.title" placeholder="标题"></el-input>
+                    </el-form-item>
+                    <br />
+                    <el-form-item label="内容：">
+                        <el-input v-model="formInline.content" placeholder="内容"></el-input>
+                    </el-form-item>
+                    <br />
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit('formInline')">保存</el-button>
+                        <el-button type="primary" @click="closeAdd1('formInline')">取消</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
         </div>
-        <el-form :inline="true" :model="formInline" :rules="rules" ref="formInline" class="demo-form-inline">
-          <el-form-item label="方案名称：">
-            <el-input v-model="formInline.cfgName" placeholder="方案名称"></el-input>
-          </el-form-item>
-          <div class="bt">
-            <div class="smallTitle">日结方案：</div>
-            <el-form-item label="交易佣金率：" prop="dayCommission">
-              <el-input v-model="formInline.dayCommission" placeholder="交易佣金率"></el-input>
-            </el-form-item>
-            <el-form-item label="管理费成交率：" prop="dayManageFeeDealRate">
-              <el-input v-model="formInline.dayManageFeeDealRate" placeholder="管理费成交率"></el-input>
-            </el-form-item>
-          </div>
-          <div class="bt">
-            <div class="smallTitle">合作分成方案：</div>
-            <el-form-item label="交易佣金率：" prop="singleCommission">
-              <el-input v-model="formInline.singleCommission" placeholder="交易佣金率"></el-input>
-            </el-form-item>
-            <el-form-item label="管理费成交率：" prop="singleManageFeeDealRate">
-              <el-input v-model="formInline.singleManageFeeDealRate" placeholder="管理费成交率"></el-input>
-            </el-form-item>
-            <el-form-item label="盈利分成成交率：" prop="singleDividedRate">
-              <el-input v-model="formInline.singleDividedRate" placeholder="管理费成交率"></el-input>
-            </el-form-item>
-          </div>
-          <div class="bt">
-            <div class="smallTitle">月结方案：</div>
-            <el-form-item label="交易佣金率：" prop="monthCommission">
-              <el-input v-model="formInline.monthCommission" placeholder="交易佣金率"></el-input>
-            </el-form-item>
-            <el-form-item label="管理费成交率：" prop="monthManageFeeDealRate">
-              <el-input v-model="formInline.monthManageFeeDealRate" placeholder="管理费成交率"></el-input>
-            </el-form-item>
-          </div>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit('formInline')">保存</el-button>
-            <el-button type="primary" @click="closeAdd1('formInline')">取消</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -110,75 +78,12 @@ export default {
       currentPage: 1,
       total: 10,
       nullTable: false,
-
+      title: "",
+      roleName: "",
       showAdd: false,
       formInline: {
-        cfgName: "",
-        dayCommission: "",
-        dayManageFeeDealRate: "",
-        singleCommission: "",
-        singleManageFeeDealRate: "",
-        singleDividedRate: "",
-        monthCommission: "",
-        monthManageFeeDealRate: ""
-      },
-      rules: {
-        dayCommission: [
-          { required: true, message: "请输入交易佣金率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ],
-        dayManageFeeDealRate: [
-          { required: true, message: "请输入管理费成交率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ],
-        singleCommission: [
-          { required: true, message: "请输入交易佣金率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ],
-        singleManageFeeDealRate: [
-          { required: true, message: "请输入管理费成交率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ],
-        singleDividedRate: [
-          { required: true, message: "请输入盈利分成成交率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ],
-        monthCommission: [
-          { required: true, message: "请输入交易佣金率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ],
-        monthManageFeeDealRate: [
-          { required: true, message: "请输入管理费成交率", trigger: "blur" },
-          {
-            pattern: /^(0.\d+|0|1)$/,
-            message: "请输入大于0小于等于1的数字",
-            trigger: "blur"
-          }
-        ]
+        title: "",
+        content: ""
       },
       id: "",
       addTitle: "新增"
@@ -246,45 +151,17 @@ export default {
       );
     },
     getContent(index, row) {
+      this.formInline.title = row.title;
+      this.formInline.content = row.content;
       this.id = row.id;
-      this.axios
-        .get("/tn/mgr-api/commissionCfgs/getById", {
-          params: {
-            id: this.id
-          }
-        })
-        .then(res => {
-          console.log("getFundAccount>>", res.data);
-          if (res.data.code == 200) {
-            let data = res.data.data;
-            this.showAdd = true;
-            this.addTitle = "修改";
-            this.id = data.id;
-            this.formInline.id = data.id;
-            this.formInline.cfgName = data.cfgName;
-            this.formInline.dayCommission = data.dayCommission;
-            this.formInline.dayManageFeeDealRate = data.dayManageFeeDealRate;
-            this.formInline.singleCommission = data.singleCommission;
-            this.formInline.singleManageFeeDealRate =
-              data.singleManageFeeDealRate;
-            this.formInline.singleDividedRate = data.singleDividedRate;
-            this.formInline.monthCommission = data.monthCommission;
-            this.formInline.monthManageFeeDealRate =
-              data.monthManageFeeDealRate;
-          } else {
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      this.addTitle = "修改";
+      this.showAdd = true;
     },
     deleteNow(index, row) {
-      console.log(222);
-      console.log("13点", index, row);
-      this.id = row.id;
+      let roleId = row.roleId;
       this.axios
-        .post("/tn/mgr-api/commissionCfgs/delete", {
-          id: this.id
+        .post("/tn/mgr-api/sysmgr/deleteRole", {
+          roleId: roleId
         })
         .then(res => {
           console.log("getFundAccount>>", res.data);
@@ -329,6 +206,8 @@ export default {
     showAddNow() {
       this.addTitle = "新增";
       this.showAdd = true;
+      this.formInline.title = "";
+      this.formInline.content = "";
     },
     closeAdd() {
       this.showAdd = false;
@@ -338,20 +217,17 @@ export default {
       this.showAdd = false;
     },
     onSubmit(formName) {
+      if (!this.id) {
+        this.id = "";
+      }
       this.$refs[formName].validate(valid => {
         if (valid) {
           console.log("submit!");
           this.axios
-            .post("/tn/mgr-api/commissionCfgs/saveOrUpdate", {
-              id: this.formInline.id,
-              cfgName: this.formInline.cfgName,
-              dayCommission: this.formInline.dayCommission,
-              dayManageFeeDealRate: this.formInline.dayManageFeeDealRate,
-              singleCommission: this.formInline.singleCommission,
-              singleManageFeeDealRate: this.formInline.singleManageFeeDealRate,
-              singleDividedRate: this.formInline.singleDividedRate,
-              monthCommission: this.formInline.monthCommission,
-              monthManageFeeDealRate: this.formInline.monthManageFeeDealRate
+            .post("/tn/mgr-api/sysmgr/bulltinMgr/save", {
+              id: this.id,
+              title: this.formInline.title,
+              content: this.formInline.content
             })
             .then(res => {
               console.log("getFundAccount>>", res.data);
@@ -359,6 +235,11 @@ export default {
                 this.showAdd = false;
                 this.getFundAccount();
               } else {
+                this.$alert(res.data.info, "提示", {
+                  confirmButtonText: "确定",
+                  center: true,
+                  type: "error"
+                });
               }
             })
             .catch(err => {
@@ -385,10 +266,10 @@ export default {
     },
     getFundAccount() {
       this.axios
-        .post("/tn/mgr-api/commissionCfgs/pageQuery", {
+        .post("/tn/mgr-api/sysmgr/bulltinList", {
           pageSize: this.pageSize,
           pageNo: this.currentPage,
-          cfgName: this.cfgName
+          title: this.title
         })
         .then(res => {
           console.log("getFundAccount>>", res.data);
@@ -435,8 +316,8 @@ export default {
 }
 .addContent {
   background-color: #fff;
-  width: 800px;
-  height: 530px;
+  width: 400px;
+  height: 260px;
   position: absolute;
   left: 50%;
   transform: translate(-50%, 10%);
