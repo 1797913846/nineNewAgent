19fdd6252682ae64c7f53ac15a7f81c4a89876fb
金 <1211050148@qq.com>
2020-11-03 14:25:34 +0800

    ITG撤单改成撤单

--- src/pages/ninehome/yy/y3.vue
+++ src/pages/ninehome/yy/y3.vue
@@ -74,7 +74,7 @@
             <template slot-scope="scope">
               <div class="operation">
                 <span @click.stop="set1(scope.$index, scope.row)" class="addSameClass " style="color:#f0ad4e;">修改</span>
-                <span v-if="scope.row.entruststatus!= 6" @click.stop="set2(scope.$index, scope.row)" class="addSameClass " style="color:#d9534f;">ITG撤单</span>
+                <span v-if="scope.row.entruststatus!= 6" @click.stop="set2(scope.$index, scope.row)" class="addSameClass " style="color:#d9534f;">撤单</span>
               </div>
             </template>
           </el-table-column>
