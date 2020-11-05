3752e5643be708899888936e434d824330a1ffb7
金 <1211050148@qq.com>
2020-11-05 17:54:22 +0800

    安全配置

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -9,7 +9,12 @@
         <span style="cursor: pointer;" @click="loginOut">退出</span>
       </span>
       <span class="r3">|</span>
-      <img class="r4" src="../assets/nine/chi.png" alt="">
+      <span class="r5">
+        <el-switch v-model="value1" active-text="开" inactive-text="关" @change="changeVal">
+        </el-switch>
+      </span>
+
+      <!-- <img class="r4" src="../assets/nine/chi.png" alt=""> -->
     </div>
   </div>
 </template>
@@ -18,14 +23,53 @@
 export default {
   data() {
     return {
-      userName: localStorage.getItem("userName")
+      userName: localStorage.getItem("userName"),
+      value1: true
     };
   },
   created() {},
   mounted() {
     this.userName = localStorage.getItem("userName");
+    this.getValue1();
   },
   methods: {
+    getValue1() {
+      this.axios
+        .post("/tn/mgr-api/sysmgr/get-mobile-config")
+        .then(res => {
+          if (res.data.code == 200) {
+            this.value1 = res.data.data.loginVerifyPhone;
+            if (this.value1 == 1) {
+              this.value1 = true;
+            } else {
+              this.value1 = false;
+            }
+          } else {
+          }
+        })
+        .catch(() => {});
+    },
+    changeVal(val) {
+      console.log("我是值", val);
+      if (val == false) {
+        val = 0;
+      } else if (val == true) {
+        val = 1;
+      }
+      this.axios
+        .post("/tn/mgr-api/sysmgr/save-mobile-config", {
+          loginVerifyPhone: val
+        })
+        .then(res => {
+          console.log("我啊", res);
+          if (res.data.code == 200) {
+            this.getValue1();
+            this.value1 = res.data.data.loginVerifyPhone;
+          } else {
+          }
+        })
+        .catch(() => {});
+    },
     loginOut() {
       this.axios
         .post("/tn/mgr-api/logout")
@@ -49,7 +93,7 @@ export default {
 .topnav {
   background-color: #fff;
   height: 75px;
-  margin-left:1px;
+  margin-left: 1px;
 }
 .navname {
   color: #5b5b65;
@@ -95,5 +139,8 @@ export default {
   width: 24px;
   height: 24px;
   margin-top: 25.5px;
+}
+.rightnav .r5 {
+  margin-top: 25px;
 }
 </style>
